b195a44a7f4f4f1ae8a8c7caad33aa46751764b7
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-03-16 16:50:51 +0000

    Add sample G+ edge list and stub job to preprocess it

---
+++ src/main/scala/PreProcessor.scala
@@ -0,0 +1,14 @@
+/**
+ * Author: Fayimora Femi-Balogun (fayi@fayimora.com)
+ * Date: 16/03/2014
+ * Time: 16:46
+ */
+
+import org.apache.spark.SparkContext
+import SparkContext._
+
+class PreProcessor {
+  def main(args: Array[String]) {
+
+  }
+}
